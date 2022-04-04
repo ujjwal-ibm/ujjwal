b3196a290a97910254f20cc87eb8054710255733
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2022-04-04 21:54:39 +0530

    1st

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ujjwal-ibm/ujjwal
+
+go 1.16
---
+++ hello.go
@@ -0,0 +1,8 @@
+package hello
+
+func Welcome() string {
+	return "Hello, gophers From Go 1.16 verion"
+}
+func Version() string {
+	return "1st"
+}
