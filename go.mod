1c00c2089b917c00926566fd0a0fa8c36ed1f44a
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2022-04-05 15:50:54 +0530

    final

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ujjwal-ibm/ujjwal
 go 1.16
 
 retract (
+	v1.2.0 
+	v1.1.0 
 	v1.0.0
 	v0.3.0
 	v0.2.0
--- hello.go
+++ hello.go
@@ -1,8 +1,8 @@
 package hello
 
 func Welcome() string {
-	return "Hello, 1.2 version is new"
+	return "Hello, 0.6 all retracted version is new"
 }
 func Version() string {
-	return "1.2th"
+	return "0.6th"
 }
